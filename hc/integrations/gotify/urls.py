f0974c4d617bb3a57b91c8468e9958905b082a7e
atrishmanm <atrishmanm@gmail.com>
2026-09-24 09:18:36 +0530

    Add .gitattributes (LF line endings for docker builds)

--- hc/integrations/gotify/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from django.urls import path
-
-from hc.integrations.gotify import views
-
-urlpatterns = [
-    path("projects/<uuid:code>/add_gotify/", views.add, name="hc-add-gotify"),
-]
